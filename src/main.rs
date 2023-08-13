536137eeaf23d0a11c7a0d63d69ca5b3c75cc84c
dyzio6684 <51453773+dyzio6684@users.noreply.github.com>
2023-08-13 17:26:37 +0200

    Show window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{Sandbox, Settings, window::{self, PlatformSpecific}};
+
+struct NbtEdit;
+
+impl Sandbox for NbtEdit {
+    type Message = ();
+
+    fn new() -> Self {
+        NbtEdit
+    }
+
+    fn title(&self) -> String {
+        format!("nbtedit {}", env!("CARGO_PKG_VERSION"))
+    }
+
+    fn update(&mut self, _message: Self::Message) {
+        
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        "nbtedit".into()
+    }
+}
+
+fn main() -> iced::Result {
+    NbtEdit::run(Settings {
+        window: window::Settings {
+            size: (800, 600),
+            platform_specific: PlatformSpecific {
+                application_id: format!("{}", "CARGO_PKG_NAME")
+            },
+            ..Default::default()
+        },
+        ..Default::default()
+    })
 }
